987f996fa3ff503b07557b980e05304d1d622cad
strandgeek <strandgeek@gmail.com>
2023-09-09 14:45:21 -0400

    chore(webapp): setup react-query
    Changes:
    
    * Add `@tanstack/react-query` package
    * Create initial queryClient and use the QueryClientProvider

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -3,20 +3,25 @@ import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom"
+} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 
 const router = createBrowserRouter([
-  // ROUTER - START
   {
     path: "/",
     element: <div>Hello world!</div>,
   },
-  // ROUTER - END
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+const queryClient = new QueryClient()
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
