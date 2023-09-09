34ce7bffe2a666a68fac0dabc73ea55147b43133
strandgeek <strandgeek@gmail.com>
2023-09-09 15:55:18 -0400

    chore(webapp): configure theming
    Changes:
    
    * Extend default DaisyUI theme and define the primary color for the project

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,8 +1,27 @@
+import daisyui from 'daisyui';
+import defaultTheme from 'daisyui/src/theming/themes';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.stories.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...defaultTheme["[data-theme=light]"],
+          "primary": "#1d4ed8",
+        },
+      },
+    ],
+  },
 }
